f25579ff71003e49b8b936576f9cbefb0d8b1271
Ferdinand57 <ferdinandbs77@gmail.com>
2025-02-14 20:40:18 +0700

    fix minor dependencies bug

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,11 @@ plugins {
     id("io.spring.dependency-management") version "1.1.7"
 }
 
+val seleniumJavaVersion = "4.14.1"
+val seleniumJupiterVersion = "5.0.1"
+val webdrivermanagerVersion = "5.6.3"
+val junitJupiterVersion = "5.9.1"
+
 group = "id.ac.ui.cs.advprog"
 version = "0.0.1-SNAPSHOT"
 
@@ -62,8 +67,4 @@ tasks.withType<Test>().configureEach {
 }
 
 
-val seleniumJavaVersion = "4.14.1"
-val seleniumJupiterVersion = "5.0.1"
-val webdrivermanagerVersion = "5.6.3"
-val junitJupiterVersion = "5.9.1"
 
