6bf4c860e059295a6dfc688f6f06e3fd01c39be7
Ferdinand57 <ferdinandbs77@gmail.com>
2025-03-05 19:27:07 +0700

    [RED] Add tests for Order model

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/ProductTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/ProductTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductTest {
-    //no usages
     Product product;
     @BeforeEach
     void setUp() {
